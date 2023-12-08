fb4f752e75febf2f5b231383bfeea66ee5bfa81e
Karas1ck <stasmashtakovv@mail.ru>
2023-12-08 14:29:34 +0300

    New code

---
+++ Quest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Programm
+{
+    public static void Main()
+    {
+        Random rar = new Random();
+        int[] arr = new int[10];
+        Console.WriteLine("Набор чисел:");
+        for (int c = 0; c < arr.Length; c++)
+        {
+            arr[c] = rar.Next(1, 50);
+            int a = arr[c];
+            Console.Write(a + " ");
+        }
+        int sum = arr.Sum();
+        int m = arr.Min();
+        int s = arr.Max();
+        Console.WriteLine("\nМинимальное число: " + m);
+        Console.WriteLine("Максимальное число: " + s);
+        Console.WriteLine("Сумма всех чисел: " + sum);
+    }
+}
