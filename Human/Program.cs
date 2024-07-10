0f1b72c4441e72f1871f92e47854e081222929ac
Karas1ck <stasmashtakovv@mail.ru>
2024-07-10 16:39:37 +0300

    text

---
+++ Human/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+public class Programm 
+{
+        class Human
+    {
+        public string Name = "Not";
+    }
+
+    class List
+    {
+        public Human Humans;
+        public string GetName()
+            {
+                return Humans.Name;
+            }
+        public void PushName(string Name)
+        {
+            Humans = new Human{ Name = Name };
+            }
+    }
+    static void Main(string[] args)
+    {
+        List list = new List();
+        list.PushName("Человечек");
+        Console.WriteLine(list.GetName());
+        list.PushName("Чел");
+        Console.WriteLine(list.GetName());
+    }
+}
