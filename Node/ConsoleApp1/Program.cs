558296f15f7b5249e455b8584c5782ca2a8e024a
Karas1ck <stasmashtakovv@mail.ru>
2024-08-02 15:48:05 +0300

    text

--- Node/ConsoleApp1/Program.cs
+++ Node/ConsoleApp1/Program.cs
@@ -18,13 +18,18 @@ public class Program
         public int GetValue(int index)
         {
             if (index == 0)
+            {
+                Console.WriteLine("error 0");
+                return -1;
+            }
+            if (index == 1)
                 return FirstNode.Value;
             else
             {
                 Node CurrectNode = FirstNode;
                 if (CurrectNode.Next != null)
                 {
-                    for (int i = 0; i < index; i++)
+                    for (int i = 1; i < index; i++)
                         CurrectNode = CurrectNode.Next;
                 }
                 return CurrectNode.Value;
@@ -51,6 +56,8 @@ public class Program
         public void Delete(int index)
         {
             if (index == 0)
+                Console.WriteLine("error 0");
+            if (index == 1)
                 FirstNode = FirstNode.Next;
             else
             {
@@ -58,8 +65,12 @@ public class Program
                 Node CurrectNode = FirstNode;
                 if (CurrectNode.Next != null)
                 {
-                    for (int i = 0; i < index - 1; i++)
+                    for (int i = 1; i < index - 1; i++)
                         CurrectNode = CurrectNode.Next;
+                    if (CurrectNode.Next.Next == null)
+                    {
+                        LastNode = CurrectNode;
+                    }
                 }
                 CurrectNode.Next = CurrectNode.Next.Next;
             }
@@ -73,15 +84,31 @@ public class Program
     static void Main(string[] args)
     {
         List list = new List();
+        list.Push(1);
+        list.Push(2);
+        list.Push(3);
+        list.Push(4);
+        list.Push(5);
+        Console.WriteLine(list.GetValue(1));
+        Console.WriteLine(list.GetValue(2));
+        Console.WriteLine(list.GetValue(3));
+        Console.WriteLine(list.GetValue(4));
+        Console.WriteLine(list.GetValue(5));
+        list.Delete(3);
+        Console.WriteLine(list.GetValue(1));
+        Console.WriteLine(list.GetValue(2));
+        Console.WriteLine(list.GetValue(3));
+        Console.WriteLine(list.GetValue(4));
+        Console.WriteLine(list.GetLength());
         DateTime date1;
         DateTime date2;
         date1 = DateTime.Now;
-        for (int i = 0; i <= 500000; i++)
-        {
-            list.Push(i);
-        }
+        //for (int i = 0; i <= 500000; i++)
+        //{
+        //    list.Push(i);
+        //}
         date2 = DateTime.Now;
-        Console.WriteLine("500.000 Элементов - " + date2.Subtract(date1));
+        //Console.WriteLine("500.000 Элементов - " + date2.Subtract(date1));
     }
 }
 
