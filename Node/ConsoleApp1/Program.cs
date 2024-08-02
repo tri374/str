6b96afb67802903baceba7307daeefef4cb2a5c9
Karas1ck <stasmashtakovv@mail.ru>
2024-08-02 16:15:49 +0300

    text

--- Node/ConsoleApp1/Program.cs
+++ Node/ConsoleApp1/Program.cs
@@ -29,8 +29,15 @@ public class Program
                 Node CurrectNode = FirstNode;
                 if (CurrectNode.Next != null)
                 {
-                    for (int i = 1; i < index; i++)
-                        CurrectNode = CurrectNode.Next;
+                    if (index == length)
+                    {
+                        return LastNode.Value;
+                    }
+                    else
+                    {
+                        for (int i = 1; i < index; i++)
+                            CurrectNode = CurrectNode.Next;
+                    }
                 }
                 return CurrectNode.Value;
             }
@@ -76,38 +83,28 @@ public class Program
             }
             length--;
         }
-        public int GetLength()
-        {
-            return length;
-        }
     }
     static void Main(string[] args)
     {
         List list = new List();
-        list.Push(1);
-        list.Push(2);
-        list.Push(3);
-        list.Push(4);
-        list.Push(5);
-        Console.WriteLine(list.GetValue(1));
-        Console.WriteLine(list.GetValue(2));
-        Console.WriteLine(list.GetValue(3));
-        Console.WriteLine(list.GetValue(4));
-        Console.WriteLine(list.GetValue(5));
-        list.Delete(3);
-        Console.WriteLine(list.GetValue(1));
-        Console.WriteLine(list.GetValue(2));
-        Console.WriteLine(list.GetValue(3));
-        Console.WriteLine(list.GetValue(4));
-        Console.WriteLine(list.GetLength());
         DateTime date1;
         DateTime date2;
         date1 = DateTime.Now;
-        //for (int i = 0; i <= 500000; i++)
-        //{
-        //    list.Push(i);
-        //}
+        for (int i = 1; i <= 500000; i++)
+        {
+            list.Push(i);
+        }
         date2 = DateTime.Now;
+        Console.WriteLine("500.000 Элементов - " + date2.Subtract(date1));
+        date1 = DateTime.Now;
+        Console.WriteLine(list.GetValue(500000));
+        date2 = DateTime.Now;
+        Console.WriteLine(date2.Subtract(date1));
+        date1 = DateTime.Now;
+        Console.WriteLine(list.length);
+        Console.WriteLine(list.GetValue(499999));
+        date2 = DateTime.Now;
+        Console.WriteLine(date2.Subtract(date1));
         //Console.WriteLine("500.000 Элементов - " + date2.Subtract(date1));
     }
 }
--- Node/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Node/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6821d4e23c58b9f9c7740a79221326ce5eac6b59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+558296f15f7b5249e455b8584c5782ca2a8e024a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
